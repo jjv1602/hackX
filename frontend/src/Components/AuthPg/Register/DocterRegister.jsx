b9b8771f3d0f6b608dd0f6bc599a4ba975997cb4
Divya Chakshu Bisht <65598930+DCBisht@users.noreply.github.com>
2024-02-24 17:24:08 +0530

    Frontend updates

---
+++ frontend/src/Components/AuthPg/Register/DocterRegister.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Divider } from "primereact/divider";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { FileUpload } from 'primereact/fileupload';
+import st from "./Register.module.css";
+import { InputMask } from 'primereact/inputmask';
+
+
+const DocterRegistern = () => {
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [phNo, setPhNo] = useState("");
+  const [img, setimg] = useState();
+  const navigate = useNavigate();
+  return (
+    <div className={st.card}>
+      <div className="flex flex-column md:flex-row">
+        <div className="w-full md:w-10 flex flex-column align-items-center justify-content-center gap-5 py-5">
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-6rem">Name</label>
+            <InputText id="username" type="email" className="w-20rem" />
+          </div>
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-6rem">Email</label>
+            <InputText id="email" type="email" className="w-20rem" />
+          </div>
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-6rem">Password</label>
+            <InputText id="password" type="password" className="w-20rem" />
+          </div>
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-6rem">User Image</label>
+            <FileUpload name="demo[]" url={'/api/upload'} multiple accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} />
+          </div>
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-6rem">Speciality</label>
+            <InputText id="speciality" type="string" className="w-20rem" />
+          </div>
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-6rem">Introduction</label>
+            <InputText id="password" type="string" className="w-20rem" />
+          </div>
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-6rem">Year of Experirnce</label>
+            <InputText id="yoe" type="string" className="w-20rem" />
+          </div>
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-6rem">Contact Number</label>
+            <InputMask value="ah" onChange={(e) => setValue(e.target.value)} mask="99-999999" placeholder="99-999999" />
+          </div>
+          
+          <Button
+            label="Register"
+            icon="pi pi-user"
+            className="w-10rem mx-auto"
+          ></Button>
+        </div>
+        <div className="w-full md:w-5 flex flex-row align-items-center justify-content-center gap-5 py-5">
+          <Divider layout="vertical" className="hidden md:flex">
+            <b>OR</b>
+          </Divider>
+          <Divider
+            layout="horizontal"
+            className="flex md:hidden"
+            align="center"
+          >
+            <b>OR</b>
+          </Divider>
+        </div>
+        <div className="w-full md:w-5 flex flex-column align-items-center justify-content-center gap-5 py-5">
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-60rem">Register as a Patient</label>
+          </div>
+          <div className="w-full md:w-5 flex align-items-center justify-content-center py-5">
+            <Link to="/PatientRegister">
+              <Button
+                label="Patient Registration"
+                icon="pi pi-user-plus"
+                severity="success"
+                className="w-10rem"
+              ></Button>
+            </Link>
+          </div>
+          <div className="flex flex-wrap justify-content-center align-items-center gap-2">
+            <label className="w-60rem">Back to Login</label>
+          </div>
+          <div className="w-full md:w-5 flex align-items-center justify-content-center py-5">
+            <Link to="/Login">
+              <Button
+                label="LogIn"
+                icon="pi pi-user-plus"
+                severity="success"
+                className="w-10rem"
+              ></Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DocterRegistern;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'primeicons/primeicons.css';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeflex/primeflex.css';  
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PrimeReactProvider>
     <App />
-  </React.StrictMode>,
+    </PrimeReactProvider>
+  </React.StrictMode>
 )
